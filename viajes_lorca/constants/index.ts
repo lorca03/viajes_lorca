198a6e44ba59989b1bb2d997f395f2096c50207a
lorca03 <guillermolorcamartinez@gmail.com>
2024-06-07 14:22:20 +0200

    All the web

---
+++ viajes_lorca/constants/index.ts
@@ -0,0 +1,79 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'services', label: 'Servicios' },
+  { href: '/', key: 'pricing ', label: 'Precios ' },
+  { href: '/', key: 'contact_us', label: 'Contactanos' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Mapas reales pueden estar sin conexión',
+    icon: '/map.svg',
+    variant: 'green',
+    description:
+      'Proporcionamos una solución para que puedas usar nuestra aplicación al escalar, sí, mapas sin conexión que puedes usar en cualquier momento en que no haya señal en el lugar',
+  },
+  {
+    title: 'Establece un horario de aventura',
+    icon: '/calendar.svg',
+    variant: 'green',
+    description:
+      'Programa una aventura con amigos. En días festivos, hay muchas ofertas interesantes de ViajesLorca. De esta manera, no hay más discusiones',
+  },
+  {
+    title: 'Tecnología que utiliza realidad aumentada',
+    icon: '/tech.svg',
+    variant: 'green',
+    description:
+      'La tecnología utiliza realidad aumentada como guía para tu sendero de senderismo en el bosque hasta la cima de la montaña. Ya es compatible con la última tecnología sin conexión a internet',
+  },
+  {
+    title: 'Muchas ubicaciones nuevas cada mes',
+    icon: '/location.svg',
+    variant: 'orange',
+    description:
+      'Muchas ubicaciones nuevas cada mes, porque tenemos una comunidad mundial de escaladores que comparten sus mejores experiencias con la escalada',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+  {
+    title: 'Learn More',
+    links: [
+      'Sobre Viajes Lorca',
+      'Blog',
+      'Trabajos', 
+      'Politica de privacidad',
+      'Contactanos',
+    ],
+  },
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Info Contacto',
+  links: [
+    { label: 'Movil Creador', value: '666 666 666' },
+    { label: 'Email Creador', value: 'ventas@lorca.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Redes',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg', // cambiar por la x
+    '/youtube.svg',
+  ],
+};
