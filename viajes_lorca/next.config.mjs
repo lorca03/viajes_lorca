b5f3acb5602d2d779e0f065cfaa9a5d567978089
lorca03 <guillermolorcamartinez@gmail.com>
2024-06-08 10:41:17 +0200

    Update next.config.mjs

--- viajes_lorca/next.config.mjs
+++ viajes_lorca/next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    webpack: (config) => {
+        config.resolve.alias["@"] = path.resolve(__dirname);
+        return config;
+    },
     output: 'export',
     images: { unoptimized: true },
 };
